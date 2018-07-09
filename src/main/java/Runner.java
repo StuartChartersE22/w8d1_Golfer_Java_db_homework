8d6da7fffb71ff616d0f7aab76ea4a9b78d5e6d3
StuartCharters <charters.stuart@gmail.com>
2018-07-09 16:41:41 +0100

    can save a golfer to the db using the runner

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,11 +1,16 @@
 
 
+import db.DBGolfer;
+import models.Golfer;
+
 import java.util.List;
 
 public class Runner {
 
     public static void main(String[] args) {
 
+        Golfer golfer = new Golfer("John", "Billson", 47);
+        DBGolfer.save(golfer);
 
         System.exit(0);
     }
--- src/main/java/models/Golfer.java
+++ src/main/java/models/Golfer.java
@@ -1,4 +1,59 @@
 package models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "golfers")
 public class Golfer {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Golfer(){}
+
+    public Golfer(String firstName, String lastName, int age){
+        this.age = age;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Column(name = "age")
+    public int getAge() {
+        return age;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    @Column(name = "first_name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Column(name = "last_name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
